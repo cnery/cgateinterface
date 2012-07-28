8238abc44450367b3bae3aaefa12e13a2277617f
Dave Oxley <dave@daveoxley.co.uk>
2012-07-28 14:34:03 +1000

    Output emptry git.branch info if we're on a tag.

--- git-status.sh
+++ git-status.sh
@@ -3,7 +3,9 @@ echo "git.commit=`git rev-parse --short HEAD`" > git-status.properties
 tag=`git describe --tags`
 if [ $? == 0 ]; then
     echo "git.tag=$tag" >> git-status.properties
+    echo "git.branch=" >> git-status.properties
 else
+    echo "git.tag=" >> git-status.properties
     echo "git.branch=`git branch 2>/dev/null| sed -n '/^\*/s/^\* //p'`" >> git-status.properties
 fi
 if git diff --quiet 2>/dev/null >&2; then
